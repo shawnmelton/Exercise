e8f381a8b16addda2cc1a813130051873bb044cd
gejustin <gejustin1868@gmail.com>
2014-04-28 16:44:35 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿window.onload = function() {
+    var overlay = document.querySelector('.overlay'),
+        overlayContent = document.querySelector('.overlay-content');
+
+    setTimeout(function() {
+        overlay.className = "overlay";
+        overlayContent.className += " center";
+        setTimeout(function() {
+            overlay.className += " isHidden";
+            overlayContent.className += " isHidden";
+        }, 5000);
+    },3000);
+};
